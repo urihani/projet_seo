baeab3571a232a652afdf3571dc2b7b4492f6c2d
estelleJoyb <estelle.joyb@gmail.com>
2023-01-31 11:43:06 +0100

    test

---
+++ js/index.js
@@ -0,0 +1,19 @@
+function onload() {
+  var consent = localStorage.getItem("acceptcookie");
+  if (consent == "oui") {
+    var head = document.getElementsByTagName("head");
+    head.innerHtml +=
+      "<script async src=\"https://www.googletagmanager.com/gtag/js?id=G-HPND2940FW\"></script><script>window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-HPND2940FW');</script>";
+  } else {
+    document.getElementById("exampleModal").modal("show");
+  }
+
+  localStorage.getItem("acceptcookie");
+}
+
+function addCookies() {
+  localStorage.setItem("acceptcookie", "oui");
+  var head = document.getElementsByTagName("head");
+  head.innerHtml +=
+    "<script async src=\"https://www.googletagmanager.com/gtag/js?id=G-HPND2940FW\"></script><script>window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-HPND2940FW');</script>";
+}
