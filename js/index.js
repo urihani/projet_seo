4273bb4905ee19f7c3eba44c349e6b2cc0e298ec
estelleJoyb <estelle.joyb@gmail.com>
2023-02-01 10:13:02 +0100

    consent

--- js/index.js
+++ js/index.js
@@ -1,19 +1,41 @@
-function onload() {
-  var consent = localStorage.getItem("acceptcookie");
-  if (consent == "oui") {
-    var head = document.getElementsByTagName("head");
-    head.innerHtml +=
-      "<script async src=\"https://www.googletagmanager.com/gtag/js?id=G-HPND2940FW\"></script><script>window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-HPND2940FW');</script>";
-  } else {
-    document.getElementById("exampleModal").modal();
-  }
+// On déclare la variable pour stocker le consentement de l'utilisateur
+let userConsent = false;
 
-  localStorage.getItem("acceptcookie");
+// On définit une fonction pour demander le consentement de l'utilisateur
+function getConsent() {
+  // Afficher une boîte de dialogue pour demander le consentement
+  let consent = confirm(
+    "Acceptez-vous d'envoyer des données à Google Analytics ?"
+  );
+
+  // Mettre à jour la variable userConsent en fonction de la réponse de l'utilisateur
+  userConsent = consent ? true : false;
+
+  // On stocke le consentement de l'utilisateur dans le localStorage
+  localStorage.setItem("userConsent", userConsent);
+
+  return userConsent;
 }
 
-function addCookies() {
-  localStorage.setItem("acceptcookie", "oui");
+// On vérifie si l'utilisateur a déjà donné son consentement
+if (localStorage.getItem("userConsent")) {
+  userConsent = localStorage.getItem("userConsent");
+} else {
+  // Si l'utilisateur n'a pas encore donné son consentement, on le demande
+  userConsent = getConsent();
+}
+
+// Si l'utilisateur a donné son consentement, on initialise Google Analytics
+if (userConsent === "true") {
   var head = document.getElementsByTagName("head");
   head.innerHtml +=
     "<script async src=\"https://www.googletagmanager.com/gtag/js?id=G-HPND2940FW\"></script><script>window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-HPND2940FW');</script>";
+
+  window.dataLayer = window.dataLayer || [];
+  function gtag() {
+    dataLayer.push(arguments);
+  }
+  gtag("js", new Date());
+
+  gtag("config", "G-HPND2940FW");
 }
